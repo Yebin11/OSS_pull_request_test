5b73fdf497271b810c5d626e0f136e2e80a5ab5d
HanWool Huh <hhw0925@naver.com>
2020-11-25 11:19:50 +0900

    [Edit] Can get input.

--- hello.c
+++ hello.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 int main(){
+	char name[10];
 	
-	printf("Hello I'm Yebin\n");
+	printf("당신의 이름은 무엇입니까?\n");
+	scanf("%s", name);
+	printf("Hello I'm %s\n", name);
 
 	return 0;
 }
